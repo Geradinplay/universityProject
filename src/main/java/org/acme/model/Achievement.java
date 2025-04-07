5e7f94c02c81fb2a39f7e70a6d398dc9a2aa8c7f
Geradine <ehfy2005@gmail.com>
2025-04-07 22:46:36 +0300

    Drugi domaci zadatak Notes for the Professor: A link has been added between the user and the achievements
    Comment:
    Added achievement system, oneToMany.

--- src/main/java/org/acme/model/Achievement.java
+++ src/main/java/org/acme/model/Achievement.java
@@ -1,6 +1,9 @@
 package org.acme.model;
 
 import jakarta.persistence.*;
+
+import java.util.List;
+
 @NamedQueries({
         @NamedQuery(name = Achievement.GET_ALL_ACHIEVEMENT, query = "SELECT i FROM Achievement i")
 })
@@ -13,7 +16,11 @@ public class Achievement {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "achievement_seq")
     private Long id;
+    @JoinColumn(name = "achievement")
     private String achievement;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Long getId() {
         return id;
